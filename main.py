02213200a287551be2d139d67ce5232473551b7d
victor <victorjulioch777@gmail.com>
2026-06-07 15:39:59 -0600

    Integra menu principal con sistema local, cliente y servidor

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from ui.menu_local import mostrar_menu_local
+from server.server import iniciar_servidor
+from client.cliente import iniciar_cliente
 
 def main():
-    mostrar_menu_local()
-    
+    while True:
+        print("\n==== SISTEMA DISTRIBUIDO DE ASADAS ===")
+        print("1. Sistema local")
+        print("2. Iniciar servidor")
+        print("3. Iniciar cliente")
+        print("4. Salir")
+        
+        opcion = input("Seleccione una opcion: ").strip()
+        
+        if opcion == "1":
+            mostrar_menu_local()
+        elif opcion == "2":
+            iniciar_servidor()
+        elif opcion == "3":
+            iniciar_cliente()
+        elif opcion == "4":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opcion invalida.")
+            
 if __name__ == "__main__":
     main()
